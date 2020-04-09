c1fbced3171475ec9e4c34118ba536d96cabe6ae
Dazhi <ch2ohch2oh@gmail.com>
2020-04-09 15:59:36 -0400

    Updated overview page using new db interface.

--- app/views/overview.py
+++ app/views/overview.py
@@ -7,7 +7,7 @@ from flask import render_template, flash, redirect, url_for, request, jsonify, s
 from flask_login import current_user, login_user, logout_user, login_required
 from datetime import datetime
 
-from app.db import get_db
+from app.db import get_db, query
 
 import numpy as np
 import pandas as pd
@@ -38,10 +38,9 @@ def overview():
     rev_fig.yaxis.axis_label_text_font_size = '20pt'
     trend_script, trend_div = components(rev_fig)
     
-    
     cat_data = get_revenue_by_category(date_start, date_end)
     cat_fig = figure(x_range = cat_data.category, sizing_mode='scale_width', height=200)
-    print(f'cat_data:\n {cat_data.head()}')
+    # print(f'cat_data:\n {cat_data.head()}')
     cat_fig.vbar(x=cat_data.category, top=cat_data.revenue, width=0.9)
     cat_js, cat_div = components(cat_fig)
 
@@ -69,45 +68,33 @@ def get_total_orders(date_start, date_end):
     """
     Return the total number of orders within given time range.
     """
-    db = get_db()
-    cur = db.cursor()
-    rows = list(cur.execute(
-        f"select count(*) from sales where salesdate between to_date({date_start}, 'YYYYMMDD') and to_date({date_end}, 'YYYYMMDD')"))
-    print(rows)
-    data = dict(total_orders=rows[0][0])
-    return jsonify(data)
+    sql = f"select count(*) from sales where salesdate between to_date('{date_start}', 'YYYY-MM-DD') and to_date('{date_end}', 'YYYY-MM-DD')"
+    rows = query(sql)
+    return rows[0][0]
 
 
-@app.route('/api/total_revenue', methods=['GET'])
 def get_total_revenue(date_start, date_end):
     """
     Return the total revenue within given time range.
     """
     db = get_db()
     cur = db.cursor()
-    rows = list(cur.execute(
-        f"select sum(total) from sales where salesdate between to_date({date_start}, 'YYYYMMDD') and to_date({date_end}, 'YYYYMMDD')"))
-    print(rows)
-    data = dict(total_revenue=rows[0][0])
-    return jsonify(data)
-
-def revenue_by_time():
-    sql = f"select sum(total) from sales where salesdate between to_date('{date_start}', 'YYYY-MM-DD') and to_date('{date_end}', 'YYYY-MM-DD')"
-    print(f'SQL: {sql}')
-    rows = list(cur.execute(sql))
-    print(rows)
+    sql =  f"select sum(total) from sales where salesdate between to_date('{date_start}', 'YYYY-MM-DD') and to_date('{date_end}', 'YYYY-MM-DD')"
+    rows = query(sql)
     return rows[0][0]
 
 def get_revenue_by_time(date_start, date_end):
     """
     Return the revenue for each day within the time range.
     """
-    db = get_db()
-    cur = db.cursor()
+    sql = f"""
+    select salesdate, sum(total) from sales 
+    where salesdate between to_date('{date_start}', 'YYYY-MM-DD') and to_date('{date_end}', 'YYYY-MM-DD') 
+    group by salesdate order by salesdate
+    """
+    rows = query(sql)
 
-    rows = cur.execute(f"select salesdate, sum(total) from sales where salesdate between to_date('{date_start}', 'YYYY-MM-DD') and to_date('{date_end}', 'YYYY-MM-DD') group by salesdate order by salesdate")
     df = pd.DataFrame(columns=['date', 'revenue'])
-
     for row in rows:
         df.loc[len(df), :] = row
     df['date'] = pd.to_datetime(df['date'])
@@ -117,17 +104,17 @@ def get_revenue_by_category(date_start, date_end):
     """
     Return the total revenue for each category within the time range.
     """
-    db = get_db()
-    cur = db.cursor()
-    rows = cur.execute(
-        f"""
-        select productcategory.name, sum(sales.total)
-        from sales, product, productcategory
-        where salesdate between to_date('{date_start}', 'YYYY-MM-DD') and to_date('{date_end}', 'YYYY-MM-DD') 
-            and sales.productID = product.productID 
-            and product.productID = productcategory.categoryID
-        group by productcategory.name
-        order by sum(sales.total) desc""")  
+
+    sql = f"""
+    select productcategory.name, sum(sales.total)
+    from sales, product, productcategory
+    where salesdate between to_date('{date_start}', 'YYYY-MM-DD') and to_date('{date_end}', 'YYYY-MM-DD') 
+        and sales.productID = product.productID 
+        and product.productID = productcategory.categoryID
+    group by productcategory.name
+    order by sum(sales.total) desc"""
+    rows = query(sql)
+
     df = pd.DataFrame(columns=['category', 'revenue'])
     for row in rows:
         df.loc[len(df), :] = row
