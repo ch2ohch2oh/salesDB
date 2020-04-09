d50c32e2575c3232fc26ecd260dd052e7a851bd4
Dazhi <ch2ohch2oh@gmail.com>
2020-04-09 00:24:47 -0400

    Finished overview page.

--- app/views/overview.py
+++ app/views/overview.py
@@ -2,58 +2,53 @@ from app import app
 from bokeh.embed import components
 from bokeh.plotting import figure
 from bokeh.resources import INLINE
-from flask import render_template, flash, redirect, url_for, request, jsonify
+from bokeh.models import ColumnDataSource, HoverTool
+from flask import render_template, flash, redirect, url_for, request, jsonify, session
 from flask_login import current_user, login_user, logout_user, login_required
 from datetime import datetime
 
 from app.db import get_db
 
+import numpy as np
+import pandas as pd
+
 @app.route('/overview', methods=['GET', 'POST'])
 @login_required
 def overview():
-    """Render overview page"""
+    """Render overview page
+    
+    TODO: tooltips not working
+    """
+    
+    date_start = request.form.get('date_start', '2018-01-01')
+    date_end = request.form.get('date_end', '2018-03-12')
 
-    if app.debug:
-        print(request.form)
-    
-    # date_start = request.form['date_start']
-    # date_end = request.form['date_end']
-    # if date_start == '':
-    date_start='1234-11-11'
-    # if date_end == '':
-    date_end='4321-11-11'
-    
-    # init a basic bar chart:
-    # http://bokeh.pydata.org/en/latest/docs/user_guide/plotting.html#bars
-    rev_fig = figure(sizing_mode='scale_width')
-    rev_fig.vbar(
-        x=[1, 2, 3, 4],
-        width=0.5,
-        bottom=0,
-        top=[1.7, 2.2, 4.6, 3.9],
-        color='navy'
-    )
+    print(f'date_start = {date_start}')
+    print(f'date_end = {date_end}')
+
+    # render template
+    total_sales = get_total_revenue(date_start, date_end)
+    total_orders = get_total_orders(date_start, date_end)
+
+    rev_source = ColumnDataSource(get_revenue_by_time(date_start, date_end))
+    rev_fig = figure(sizing_mode='scale_width', x_axis_type='datetime', height=200,
+        tooltips=[('date', '$date'), ('revenue', '$revenue')])
+    rev_fig.line(x='date', y='revenue', source=rev_source)
+    rev_fig.xaxis.axis_label_text_font_size = '20pt'
+    rev_fig.yaxis.axis_label_text_font_size = '20pt'
     trend_script, trend_div = components(rev_fig)
     
-    cat_fig = figure(sizing_mode='scale_width')
-    cat_fig.vbar(
-        x=[1, 2, 3, 4],
-        width=0.5,
-        bottom=0,
-        top=[2.7, 1.2, 3.6, 1.9],
-        color='navy'
-    )
+    
+    cat_data = get_revenue_by_category(date_start, date_end)
+    cat_fig = figure(x_range = cat_data.category, sizing_mode='scale_width', height=200)
+    print(f'cat_data:\n {cat_data.head()}')
+    cat_fig.vbar(x=cat_data.category, top=cat_data.revenue, width=0.9)
     cat_js, cat_div = components(cat_fig)
 
     # grab the static resources
     js_resources = INLINE.render_js()
     css_resources = INLINE.render_css()
-
-    # render template
-    total_sales = 123123
-    total_orders = 321321
-
-    
+ 
     html = render_template(
         'overview.html',
         trend_script=trend_script,
@@ -69,68 +64,60 @@ def overview():
     )
     return html
 
-@app.route('/api/total_orders', methods=['GET'])
-def total_orders():
+def get_total_orders(date_start, date_end):
     """
     Return the total number of orders within given time range.
     """
-    date_start = request.args.get('date_start')
-    date_end = request.args.get('date_end')
     db = get_db()
     cur = db.cursor()
-    rows = list(cur.execute(f"select count(*) from sales where salesdate between to_date({date_start}, 'YYYYMMDD') and to_date({date_end}, 'YYYYMMDD')"))
-    print(rows)
-    data = dict(total_orders = rows[0][0])
-    return jsonify(data)
+    sql = f"select count(*) from sales where salesdate between to_date('{date_start}', 'YYYY-MM-DD') and to_date('{date_end}', 'YYYY-MM-DD')"
+    print(f'SQL: {sql}')
+    rows = list(cur.execute(sql))
 
-@app.route('/api/total_revenue', methods=['GET'])
-def total_revenue():
+    print(rows)
+    return rows[0][0]
+
+def get_total_revenue(date_start, date_end):
     """
     Return the total revenue within given time range.
     """
-    date_start = request.args.get('date_start')
-    date_end = request.args.get('date_end')
     db = get_db()
     cur = db.cursor()
-    rows = list(cur.execute(f"select sum(total) from sales where salesdate between to_date({date_start}, 'YYYYMMDD') and to_date({date_end}, 'YYYYMMDD')"))
+    sql = f"select sum(total) from sales where salesdate between to_date('{date_start}', 'YYYY-MM-DD') and to_date('{date_end}', 'YYYY-MM-DD')"
+    print(f'SQL: {sql}')
+    rows = list(cur.execute(sql))
     print(rows)
-    data = dict(total_revenue = rows[0][0])
-    return jsonify(data)
+    return rows[0][0]
 
-@app.route('/api/revenue_by_time', methods=['GET'])
-def revenue_by_time():
+def get_revenue_by_time(date_start, date_end):
     """
     Return the revenue for each day within the time range.
     """
-    date_start = request.args.get('date_start')
-    date_end = request.args.get('date_end')
     db = get_db()
     cur = db.cursor()
-    rows = cur.execute(f"select salesdate, sum(total) from sales where salesdate between to_date({date_start}, 'YYYYMMDD') and to_date({date_end}, 'YYYYMMDD') group by salesdate order by salesdate")
-    data = []
+    rows = cur.execute(f"select salesdate, sum(total) from sales where salesdate between to_date('{date_start}', 'YYYY-MM-DD') and to_date('{date_end}', 'YYYY-MM-DD') group by salesdate order by salesdate")
+    df = pd.DataFrame(columns=['date', 'revenue'])
     for row in rows:
-        data.append({'salesdate': row[0], 'revenue': row[1]})
-    return jsonify(data)
+        df.loc[len(df), :] = row
+    df['date'] = pd.to_datetime(df['date'])
+    return df
 
-@app.route('/api/revenue_by_cat', methods=['GET'])
-def revenue_by_cat():
+def get_revenue_by_category(date_start, date_end):
     """
     Return the total revenue for each category within the time range.
     """
-    date_start = request.args.get('date_start')
-    date_end = request.args.get('date_end')
     db = get_db()
     cur = db.cursor()
     rows = cur.execute(
         f"""
         select productcategory.name, sum(sales.total) 
         from sales, product, productcategory
-        where salesdate between to_date({date_start}, 'YYYYMMDD') and to_date({date_end}, 'YYYYMMDD') 
+        where salesdate between to_date('{date_start}', 'YYYY-MM-DD') and to_date('{date_end}', 'YYYY-MM-DD') 
             and sales.productID = product.productID 
             and product.productID = productcategory.categoryID
         group by productcategory.name
         order by sum(sales.total) desc""")  
-    data = []
+    df = pd.DataFrame(columns=['category', 'revenue'])
     for row in rows:
-        data.append({'cat_name': row[0], 'revenue': row[1]})
-    return jsonify(data)
+        df.loc[len(df), :] = row
+    return df
