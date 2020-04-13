cc0e52d704e1256980cdab9321fe657174633107
Zhang Dong <zhangdong.1995@outlook.com>
2020-04-12 22:37:24 -0400

    add gender and hometown distribution analysis to employee

--- app/views/employees.py
+++ app/views/employees.py
@@ -1,12 +1,17 @@
 from app import app
+from bokeh.io import show
 from bokeh.embed import components
 from bokeh.plotting import figure
 from bokeh.resources import INLINE
 from bokeh.models import NumeralTickFormatter
 from bokeh.models import ColumnDataSource, HoverTool
+from bokeh.palettes import Category20c
+from bokeh.transform import cumsum
 from flask import render_template, flash, redirect, url_for, request, jsonify
 from flask_login import current_user, login_user, logout_user, login_required
 from datetime import datetime
+
+from math import pi
 
 from app.db import get_db, query
 
@@ -51,7 +56,7 @@ def employees():
     revenue_total_hover = HoverTool(tooltips=[('Employee', '@name'), ('Revenue', '@revenue{$ 0.00 a}')])
     revenue_total_fig = figure(sizing_mode='scale_width', height=300, y_range=revenue_total.name,
         x_range=(revenue_total.revenue.min() - revenue_range/10, revenue_total.revenue.max()), 
-        tools=[revenue_total_hover])
+        tools=[revenue_total_hover], toolbar_location=None,)
     revenue_total_fig.hbar(y='name', right='revenue', source=revenue_total_source, height=0.8, 
         hover_color='red', hover_fill_alpha=0.8)
     # styling visual
@@ -64,6 +69,7 @@ def employees():
     revenue_total_fig.xaxis.major_label_text_font_size = '11pt'
     revenue_total_fig.yaxis.major_label_text_font_size = '11pt'
     revenue_total_fig.xaxis[0].formatter = NumeralTickFormatter(format="$ 0.00 a")
+    js_revenue_total, div_revenue_total = components(revenue_total_fig)
 
     # most orders
     orders_total = get_employee_orders_total(date_start, date_end)
@@ -75,7 +81,7 @@ def employees():
     orders_total_hover = HoverTool(tooltips=[('Employee', '@name'), ('Order number', '@orders{0.00 a}')])
     orders_total_fig = figure(sizing_mode='scale_width', height=300,
         y_range=orders_total.name, x_range=(orders_total.orders.min() - orders_range/10, orders_total.orders.max()),
-        tools=[orders_total_hover])
+        tools=[orders_total_hover], toolbar_location=None,)
     orders_total_fig.hbar(y='name', right='orders', source=orders_total_source, height=0.8, 
         hover_color='red', hover_fill_alpha=0.8)
     # styling visual
@@ -88,13 +94,45 @@ def employees():
     orders_total_fig.xaxis.major_label_text_font_size = '11pt'
     orders_total_fig.yaxis.major_label_text_font_size = '11pt'
     orders_total_fig.xaxis[0].formatter = NumeralTickFormatter(format="0.00 a")
+    js_orders_total, div_orders_total = components(orders_total_fig)
     
+    # gender relation distribution in order
+    g = get_ec_gender(date_start, date_end)
+    gender = pd.Series(g).reset_index(name='orders').rename(columns={'index':'gender'})
+    gender['angle'] = gender['orders']/gender['orders'].sum() * 2*pi
+    gender['color'] = Category20c[len(g)]
+    gender_hover = HoverTool(tooltips=[('Gender', '@gender'), ('Order number', '@orders{0.00 a}')])
+    gender_fig = figure(sizing_mode='scale_width', height=400, toolbar_location=None,
+            tools=[gender_hover], x_range=(-0.5, 1.0))
+    gender_fig.wedge(x=0, y=1, radius=0.4,
+        start_angle=cumsum('angle', include_zero=True), end_angle=cumsum('angle'),
+        line_color="white", fill_color='color', legend='gender', source=gender, 
+        hover_color='red', hover_fill_alpha=0.8)
+    gender_fig.axis.axis_label=None
+    gender_fig.axis.visible=False
+    gender_fig.grid.grid_line_color = None
+    js_gender, div_gender = components(gender_fig)
+
+    # state relation distribution in order
+    s = get_ec_state(date_start, date_end)
+    print(s)
+    state = pd.Series(s).reset_index(name='orders').rename(columns={'index':'state'})
+    state['angle'] = state['orders']/state['orders'].sum() * 2*pi
+    state['color'] = ["#3182bd", "#9ecae1"]
+    state_hover = HoverTool(tooltips=[('State', '@state'), ('Order number', '@orders{0.00 a}')])
+    state_fig = figure(sizing_mode='scale_width', height=400, toolbar_location=None,
+           tools=[state_hover], x_range=(-0.5, 1.0))
+    state_fig.wedge(x=0, y=1, radius=0.4,
+        start_angle=cumsum('angle', include_zero=True), end_angle=cumsum('angle'),
+        line_color="white", fill_color='color', legend='state', source=state, 
+        hover_color='red', hover_fill_alpha=0.8)
+    state_fig.axis.axis_label=None
+    state_fig.axis.visible=False
+    state_fig.grid.grid_line_color = None
+    js_state, div_state = components(state_fig)
 
     js_resources = INLINE.render_js()
     css_resources = INLINE.render_css()
-
-    js_revenue_total, div_revenue_total = components(revenue_total_fig)
-    js_orders_total, div_orders_total = components(orders_total_fig)
 
     html = render_template(
         'employees.html',
@@ -104,6 +142,10 @@ def employees():
         js_revenue_total=js_revenue_total,
         div_orders_total=div_orders_total,
         js_orders_total=js_orders_total,
+        js_gender=js_gender,
+        div_gender=div_gender,
+        js_state=js_state,
+        div_state=div_state,
         most_orders_name=most_orders_name,
         most_revenue_name=most_revenue_name,
         avg_order=avg_order,
@@ -173,25 +215,46 @@ def get_avg_selling_per(date_start, date_end):
     return df
 
 
-# optional and under construction
-@app.route('/api/top5_employee_monthly', methods=['GET'])
-def top5_employee_monthly():
+def get_ec_gender(date_start, date_end):
     """
-    Return the monthly order numbers and revenue trending of 2018 year top 5 employee ranked by revenue.
+    Return the order number for each gender distribution.
     """
-    date_start = 20180101
-    date_end = 20181231
-    db = get_db()
-    cur = db.cursor()
-    rows = list(cur.execute(
-        f"""
-        select employee.name as name, count(sales.salesID) as order_number, sum(sales.total) as revenue
-        from sales, employee
-        where salesdate between to_date({date_start}, 'YYYYMMDD') and to_date({date_end}, 'YYYYMMDD')
-            and sales.employeeID = employee.employeeID
-        group by employee.name
-        order by sum(sales.salesID) desc)"""))
-    data = []
-    for row in rows:
-        data.append({'employee': row[0], 'order number': row[1], 'revenue': row[2]})
-    return jsonify(data)
+    sql = f"""
+    select 
+        sum(case when c_gender = 'Male' and e_gender = 'M' then order_num else 0 end) as cm_em, 
+        sum(case when c_gender = 'Female' and e_gender = 'F' then order_num else 0 end) as cf_ef,
+        sum(case when c_gender = 'Female' and e_gender = 'M' then order_num else 0 end) as cf_em,
+        sum(case when c_gender = 'Male' and e_gender = 'F' then order_num else 0 end) as cm_ef
+    from (select count(salesID) as order_num, customer.gender as c_gender, employee.gender as e_gender
+          from customer, sales, employee
+          where salesdate between to_date('{date_start}', 'YYYY-MM-DD') and to_date('{date_end}', 'YYYY-MM-DD')
+              and customer.customerID = sales.customerID
+              and employee.employeeID = sales.employeeID
+          group by customer.gender, employee.gender)
+    """
+    rows = query(sql)
+    df = {'C_Male : E_Male': rows[0][0], 'C_Female : E_Female': rows[0][1], 'C_Female : E_Male': rows[0][2], 
+        'C_Male : E_Female': rows[0][3]}
+    return df
+
+
+def get_ec_state(date_start, date_end):
+    """
+    Return the order number for customer and emolpyee from same state.
+    """
+    sql = f"""
+    select 
+        sum(case when c_state = e_state then order_num else 0 end) as same_state, 
+        sum(case when c_state != e_state then order_num else 0 end) as diff_state
+    from(select count(salesID) as order_num, city1.state as c_state, city2.state as e_state
+         from customer, city city1, sales, employee, city city2
+         where salesdate between to_date('{date_start}', 'YYYY-MM-DD') and to_date('{date_end}', 'YYYY-MM-DD')
+            and customer.customerID = sales.customerID
+            and employee.employeeID = sales.employeeID
+            and customer.city = city1.cityID
+            and employee.city = city2.cityID
+         group by city1.state, city2.state)
+    """
+    rows = query(sql)
+    df = {'Same state': rows[0][0], 'Different state': rows[0][1]}
+    return df
