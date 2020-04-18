967418b1797ebe7984a1764834b4fec81dcb3743
Zhang Dong <zhangdong.1995@outlook.com>
2020-04-18 18:56:04 -0400

    Rewrite multiline plot and delete redundency fix bug

--- app/plot.py
+++ app/plot.py
@@ -2,6 +2,7 @@ from app import app
 from bokeh.embed import components
 from bokeh.models import ColumnDataSource, HoverTool, NumeralTickFormatter
 from bokeh.plotting import figure, show
+from bokeh.palettes import brewer
 
 def formatter(val, type='number'):
     if 10**3 < val < 10**6:
@@ -20,12 +21,14 @@ def line(data, x, y, y_type='number'):
     # print(x_range)
     formats = '$ 0.00 a' if y_type == 'dollar' else '0.00 a'
     if x == 'date':
-        rev_hover = HoverTool(tooltips=[('Date', '@date{%F}'), (y.capitalize(), '@'+y+'{'+formats+'}')], 
+        data_hover = HoverTool(tooltips=[('Date', '@date{%F}'), (y.capitalize(), '@'+y+'{'+formats+'}')], 
             formatters={'@date': 'datetime'})
+        data_fig = figure(x_axis_type='datetime', sizing_mode='scale_width', height=300, tools=[data_hover], 
+            toolbar_location=None)
     else:
         data_hover = HoverTool(tooltips=[(x.capitalize(), '@'+x), (y.capitalize(), '@'+y+'{'+formats+'}')])
-    data_fig = figure(x_range = x_range, sizing_mode='scale_width', height=300, tools=[data_hover], 
-        toolbar_location=None)
+        data_fig = figure(x_range = x_range, sizing_mode='scale_width', height=300, tools=[data_hover], 
+            toolbar_location=None)
     # styling visual
     data_fig.line(x=x, y=y, source=data_source, line_width=2)
     data_fig.xaxis.axis_label = x.capitalize()
@@ -47,8 +50,9 @@ def vbar(data, x, y, y_type='number'):
     data_hover = HoverTool(tooltips=[(x.capitalize(), '@'+x), (y.capitalize(), '@'+y+'{'+formats+'}')])
     data_fig = figure(x_range = x_range, sizing_mode='scale_width', height=300, tools=[data_hover], 
         toolbar_location=None)
-    # styling visual
+    # plot
     data_fig.vbar(x=x, top=y, source=data_source, width=0.9, hover_color='red', hover_fill_alpha=0.8)
+    # styling visual
     data_fig.xaxis.axis_label = x.capitalize()
     data_fig.xaxis.axis_label_text_font_size = "12pt"
     data_fig.xaxis.axis_label_standoff = 10
@@ -65,13 +69,13 @@ def hbar(data, x, y, x_type='number'):
     x_data = data.loc[:, x]
     x_range = (x_data.min() - (x_data.max()-x_data.min())/10, x_data.max())
     data_source = ColumnDataSource(data)
-    # print(x_range)
     formats = '$ 0.00 a' if x_type == 'dollar' else '0.00 a'
     data_hover = HoverTool(tooltips=[(y.capitalize(), '@'+y), (x.capitalize(), '@'+x+'{'+formats+'}')])
     data_fig = figure(y_range=y_range, x_range=x_range, sizing_mode='scale_width', height=300, tools=[data_hover], 
         toolbar_location=None)
-    # styling visual
+    # plot
     data_fig.hbar(y=y, right=x, source=data_source, height=0.8, hover_color='red', hover_fill_alpha=0.8)
+    # styling visual
     data_fig.xaxis.axis_label = x.capitalize()
     data_fig.xaxis.axis_label_text_font_size = "12pt"
     data_fig.xaxis.axis_label_standoff = 10
@@ -81,4 +85,34 @@ def hbar(data, x, y, x_type='number'):
     data_fig.xaxis.major_label_text_font_size = '10pt'
     data_fig.yaxis.major_label_text_font_size = '11pt'
     data_fig.xaxis[0].formatter = NumeralTickFormatter(format=formats)
+    return components(data_fig)
+
+def multiline(data, x, y, y_type='number', *args):
+    x_range = data.loc[:, x]
+    data_source = ColumnDataSource(data)
+    formats = '$ 0.00 a' if y_type == 'dollar' else '0.00 a'
+    if x == 'date':
+        # data_hover = HoverTool(tooltips=[('Date', '@date{%F}'), (y.capitalize(), '@'+y+'{'+formats+'}')], 
+        #    formatters={'@date': 'datetime'})
+        data_fig = figure(x_axis_type='datetime', sizing_mode='scale_width', height=300, toolbar_location=None)
+    else:
+        data_fig = figure(x_range = x_range, sizing_mode='scale_width', height=300, toolbar_location=None)
+    # plot
+    color = brewer['Spectral'][len(args)]
+    i = 0
+    for i in range(len(args)):
+        plot = data_fig.line(x=x, y=args[i], line_color=color[i], line_width=5, source=data_source)
+        data_fig.add_tools(HoverTool(renderers=[plot], tooltips=[(x.capitalize(), '@'+x), (args[i].capitalize(), '@'+args[i]+'{'+formats+'}')],
+            mode='vline'))
+        i += 1
+    # styling visual
+    data_fig.xaxis.axis_label = x.capitalize()
+    data_fig.xaxis.axis_label_text_font_size = "12pt"
+    data_fig.xaxis.axis_label_standoff = 10
+    data_fig.yaxis.axis_label = y.capitalize()
+    data_fig.yaxis.axis_label_text_font_size = "12pt"
+    data_fig.yaxis.axis_label_standoff = 10
+    data_fig.xaxis.major_label_text_font_size = '10pt'
+    data_fig.yaxis.major_label_text_font_size = '11pt'
+    data_fig.yaxis[0].formatter = NumeralTickFormatter(format=formats)
     return components(data_fig)
--- app/views/order.py
+++ app/views/order.py
@@ -9,7 +9,7 @@ from flask_login import current_user, login_user, logout_user, login_required
 from datetime import datetime
 
 from app.db import get_db, query
-from app.plot import formatter, vbar
+from app.plot import formatter, vbar, multiline
 
 import numpy as np
 import pandas as pd
@@ -36,30 +36,8 @@ def order():
 
     # Max, avg, min price for top 6 categories
     price_data = get_max_avg_min_price_by_cat(date_start, date_end)
-    price_source = ColumnDataSource(price_data)
-    # price_hover = HoverTool(tooltips=[('Category', '@category'), ('Max price', '@max_price{$ 0.00 a}'), 
-   #     ('Average price', '@avg_price{$ 0.00 a}'), ('Min price', '@min_price{$ 0.00 a}')])
-    price_fig = figure(x_range = price_data.category, sizing_mode='scale_width', height=250, toolbar_location=None,)
-    plot1 = price_fig.line(x='category', y='max_price', line_color="#1D91C0", line_width=5, source=price_source,)
-    price_fig.add_tools(HoverTool(renderers=[plot1], tooltips=[('Category', '@category'), ('Max price', '@max_price{$ 0.00 a}')],
-        mode='vline'))
-    plot2 = price_fig.line(x='category', y='avg_price', line_color="#FB8072", line_width=5, source=price_source)
-    price_fig.add_tools(HoverTool(renderers=[plot2], tooltips=[('Category', '@category'), ('Average price', '@avg_price{$ 0.00 a}')],
-        mode='vline'))
-    plot3 = price_fig.line(x='category', y='min_price', line_color="#CAB2D6", line_width=5, source=price_source)
-    price_fig.add_tools(HoverTool(renderers=[plot3], tooltips=[('Category', '@category'), ('Min price', '@min_price{$ 0.00 a}')],
-        mode='vline'))
-    # styling visual
-    price_fig.xaxis.axis_label = 'Category'
-    price_fig.xaxis.axis_label_text_font_size = "12pt"
-    price_fig.xaxis.axis_label_standoff = 10
-    price_fig.yaxis.axis_label = 'Price'
-    price_fig.yaxis.axis_label_text_font_size = "12pt"
-    price_fig.yaxis.axis_label_standoff = 10
-    price_fig.xaxis.major_label_text_font_size = '11pt'
-    price_fig.yaxis.major_label_text_font_size = '11pt'
-    price_fig.yaxis[0].formatter = NumeralTickFormatter(format="$ 0.00 a")
-    price_js, price_div = components(price_fig)
+    price_js, price_div = multiline(price_data, 'category', 'price', 'dollar', 
+        'max_price', 'avg_price', 'min_price')
 
     # grab the static resources
     js_resources = INLINE.render_js()
